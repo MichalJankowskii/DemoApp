63a945a85e9d0fe1401f0b9813f044b199908974
Michal Jankowski <jankowski.m@gmail.com>
2022-03-20 09:51:19 +0100

    Sum

---
+++ src/HelloWorld.Tests/Services/SumServiceTest.cs
@@ -0,0 +1,20 @@
+﻿namespace HelloWorld.Tests.Services
+{
+    using HelloWorld.Services;
+    using Xunit;
+
+    public class SumServiceTest
+    {
+        public void Given1and2_WhenRunSum_ThenSumEqual3()
+        {
+            // Arrange
+            SumService service = new SumService();
+
+            // Act
+            int result = service.Sum(1, 2);
+
+            // Assert
+            Assert.Equal(3, result);
+        }
+    }
+}
